7b40580cc451879b34fa86f4377eba854672b4d2
William-Richie <williamrichie181007@gmail.com>
2026-05-28 17:33:07 +0700

    Update Route, Controller, View, and add new View for Top Up

---
+++ resources/views/wallet/topup.blade.php
@@ -0,0 +1,16 @@
+<html>
+<body>
+    <h2>TOP UP WALLET</h2>
+    <form action="{{ route('wallet.topup.process') }}" method="POST">
+        @csrf
+        <div>
+            <p>Top Up Amount</p>
+            <input type="number" name="amount" id="amount" min="1000" required>
+        </div>
+        <button type="submit">Confirm</button>
+    </form>
+    <a href="{{ route('wallet.balance') }}">
+        <button>BACK</button>
+    </a>
+</body>
+</html>
