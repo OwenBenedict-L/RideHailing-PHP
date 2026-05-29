9a3e6ccb145a49bebd42980e1b0e7d35d3817593
OwenBenedict-L <owen.lukman07@gmail.com>
2026-05-29 15:59:36 +0700

    Adding configuration from bookings to drivers

--- resources/views/login/dashboardDriver.blade.php
+++ resources/views/login/dashboardDriver.blade.php
@@ -1,10 +1,11 @@
 <html>
 <body>
     <h2>Hello Driver: {{ Auth::guard('driver')->user()->name }}</h2>
+
     <ul>
         <li>[Notification]</li>
         <li>[CS]</li>
-        <li>[Active Order]</li>
+        <li><a href="{{ route('driver.orders') }}">Look For Orders [Active Order]</a></li>
     </ul>
 
     <form method="POST" action="/logout-driver">
