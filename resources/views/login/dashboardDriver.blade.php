a5603d8e04e0fcd36eb94597363fbfc9ca51aec2
SheireenSadeli <sheireensadeli@gmail.com>
2026-05-30 23:54:34 +0700

    Making new configuration to drivers

--- resources/views/login/dashboardDriver.blade.php
+++ resources/views/login/dashboardDriver.blade.php
@@ -4,8 +4,21 @@
 
     <ul>
         <li>[Notification]</li>
-        <li>[CS]</li>
-        <li><a href="{{ route('driver.orders') }}">Look For Orders [Active Order]</a></li>
+        <li>
+            <a href="{{ route('helpcenter.store') }}">
+                <button type="button">Help Center</button>
+            </a>
+        </li>
+        <li>
+            <a href="{{ route('driver.wallet.balance') }}">
+                <button type="button">Wallet</button>
+            </a>
+        </li>
+        <li>
+            <a href="{{ route('driver.orders') }}">
+                <button type="button">Look For Orders</button>
+            </a>
+        </li>
     </ul>
 
     <form method="POST" action="/logout-driver">
